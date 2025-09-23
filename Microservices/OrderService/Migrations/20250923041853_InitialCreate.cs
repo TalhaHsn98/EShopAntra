59cf98c1e15e78800299548d6b0c2eb5acafc43b
TalhaHsn98 <syedtalha.hussain98@gmail.com>
2025-09-23 00:22:00 -0400

    Migrations

---
+++ Microservices/OrderService/Migrations/20250923041853_InitialCreate.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Street1 = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    Street2 = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    State = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Zipcode = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Profile_PIC = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment_Type",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment_Type", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingCart",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCart", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCart_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User_Address",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Customer_Id = table.Column<int>(type: "int", nullable: false),
+                    Address_Id = table.Column<int>(type: "int", nullable: false),
+                    IsDefaultAddress = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User_Address", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_Address_Address_Address_Id",
+                        column: x => x.Address_Id,
+                        principalTable: "Address",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_User_Address_Customer_Customer_Id",
+                        column: x => x.Customer_Id,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment_Method",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Payment_Type_Id = table.Column<int>(type: "int", nullable: false),
+                    Provider = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    AccountNumber = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Expiry = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDefault = table.Column<bool>(type: "bit", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment_Method", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payment_Method_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Payment_Method_Payment_Type_Payment_Type_Id",
+                        column: x => x.Payment_Type_Id,
+                        principalTable: "Payment_Type",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shopping_Cart_Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cart_Id = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shopping_Cart_Item", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shopping_Cart_Item_ShoppingCart_Cart_Id",
+                        column: x => x.Cart_Id,
+                        principalTable: "ShoppingCart",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Order_Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    PaymentMethodId = table.Column<int>(type: "int", nullable: true),
+                    PaymentName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    ShippingMethod = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    ShippingAddress = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    BillAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Order_Status = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_Customer_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Order_Payment_Method_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalTable: "Payment_Method",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order_Details",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Order_Id = table.Column<int>(type: "int", nullable: false),
+                    Product_Id = table.Column<int>(type: "int", nullable: false),
+                    Product_name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order_Details", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Order_Details_Order_Order_Id",
+                        column: x => x.Order_Id,
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_CustomerId",
+                table: "Order",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_PaymentMethodId",
+                table: "Order",
+                column: "PaymentMethodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_Details_Order_Id",
+                table: "Order_Details",
+                column: "Order_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_Method_CustomerId",
+                table: "Payment_Method",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_Method_Payment_Type_Id",
+                table: "Payment_Method",
+                column: "Payment_Type_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shopping_Cart_Item_Cart_Id",
+                table: "Shopping_Cart_Item",
+                column: "Cart_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCart_CustomerId",
+                table: "ShoppingCart",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Address_Address_Id",
+                table: "User_Address",
+                column: "Address_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Address_Customer_Id",
+                table: "User_Address",
+                column: "Customer_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order_Details");
+
+            migrationBuilder.DropTable(
+                name: "Shopping_Cart_Item");
+
+            migrationBuilder.DropTable(
+                name: "User_Address");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCart");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Payment_Method");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Payment_Type");
+        }
+    }
+}
